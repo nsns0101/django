b5f40121b263fd18dac4ed021f491195a7bc07bc
nsns0101 <nsns0101@naver.com>
2019-12-23 21:59:00 +0900

    create model

--- elections/models.py
+++ elections/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# 선거 모델
+class Candidate(models.Model):      #모델 등록을 위해 models.Model을 상속받아야함
+    name = models.CharField(max_length=10)  #이름 문자의 최대길이는 10
+    introduction = models.TextField()       #정보 문자열의 길이 제한이 없음
+    area = models.CharField(max_length=15)  #국적
+    party_number = models.IntegerField(default=1)  #기호 default값은 숫자 1
