49fdcd5292756ab5aecbb6b50d1f922a05981802
nsns0101 <nsns0101@naver.com>
2019-12-24 00:00:58 +0900

    DB_data output

--- elections/views.py
+++ elections/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Candidate               #현재 파일경로의 models.py의 Candidate를 import
 
 def index(request):
-    return HttpResponse("Hello world")
-    
+    candidates = Candidate.objects.all()    #Candidate모델의 모든 행을 변수에 저장
+    str = ''
+    for candidate in candidates:
+        str += "<p>{} 기호{}번({})<br>".format(
+            candidate.name,
+            candidate.party_number,
+            candidate.area)
+        str += candidate.introduction + "</p>"
+
+    return HttpResponse(str)
