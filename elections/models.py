b7a5a4ed79e4b38428b9dabbe8820e9fd43ea5b3
nsns0101 <nsns0101@naver.com>
2019-12-24 13:00:36 +0900

    create_Model_Poll

--- elections/models.py
+++ elections/models.py
@@ -10,3 +10,15 @@ class Candidate(models.Model):      #모델 등록을 위해 models.Model을 상
 
     def __str__(self):      #python에서 object를 표현하는 문자열을 정의할 때는 __str__ 메서드를 오버라이딩함
         return self.name    #Candidate의 name로 이름을 표현
+
+#여론조사 모델
+class Poll(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    area = models.CharField(max_length = 15)
+
+#
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete = models.CASCADE)              #Poll 모델의 id를 이용
+    candidate = models.ForeignKey(Candidate, on_delete = models.CASCADE)    #후보이름
+    votes = models.IntegerField(default = 0)
