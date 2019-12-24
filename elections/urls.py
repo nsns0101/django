752be8e18062d16c8aeb6c18287ccc116c9ea254
nsns0101 <nsns0101@naver.com>
2019-12-24 13:12:55 +0900

    html_hyperlink_url

--- elections/urls.py
+++ elections/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from . import views             # .은 현재 폴더  (현재폴더의 views를 import)
 urlpatterns = [
-    path('', views.index),
+    path('', views.index),              #views.py파일의 index메서드 실행
+    path('areas/<str:area>/',views.areas),   #area뒤에 string형 모든 글을 적을 수 있음
 ]
--- elections/views.py
+++ elections/views.py
@@ -7,3 +7,8 @@ def index(request):
     candidates = Candidate.objects.all()    #Candidate모델의 모든 행을 변수에 저장
     context = {'candidates' : candidates}   #context에 'condidates'라는 key로 변수 candidates를 저장
     return render(request, 'elections/index.html', context)  #elections/index.html에 context 객체를 전달
+
+#출마지역
+def areas(request, area):   #area는 areas.html에서 하이퍼링크로 누른 area가 들어옴
+    return HttpResponse(area)
+
